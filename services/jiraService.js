a3f6474b89073697f2e3db17c9bc36e2db6561ca
Tal Bener <tbener@hotmail.com>
2025-02-18 13:35:11 +0200

    fix guess board function

--- services/jiraService.js
+++ services/jiraService.js
@@ -57,11 +57,25 @@ const JiraService = (() => {
             }
 
             const data = await response.json();
-            const board = data.values?.find(b => b.type === 'kanban' && b.location.projectKey === projectKey);
+            const boards = data.values;
+            let board;
+            if (boards?.length > 0) {
+                if (boards.length === 1) {
+                    board = boards[0];
+                } else {
+                    board = boards.find(b => b.type === 'kanban' && b.location.projectKey === projectKey);
+                    if (!board) {
+                        board = boards.find(b => b.location.projectKey === projectKey);
+                    }
+                }
+            }
 
-            return board
-                ? `${baseUrl}/jira/software/c/projects/${projectKey}/boards/${board.id}`
-                : baseUrl; // Return base URL if no board is found
+            if (board) {
+                return `${baseUrl}/jira/software/c/projects/${projectKey}/boards/${board.id}`;
+            }
+
+            // we couldn't find a relevant board
+            return baseUrl;
 
         } catch (error) {
             console.debug(`Error fetching board:`, error);
