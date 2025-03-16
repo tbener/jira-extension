0a97f4a43ccb0790071683c98d3a5c854493e73d
tal.bener <tal.bener@mdclone.com>
2025-03-16 17:58:18 +0200

    display issues in table

--- pages/popup/fillTable.js
+++ pages/popup/fillTable.js
@@ -22,6 +22,8 @@ export function fillIssuesTable(issuesList, containerElement) {
         const issueElement = issueTemplate.cloneNode(true);
         issueElement.classList.remove("d-none");
 
+        issueElement.setAttribute("data-issue-key", issue.key);
+
         issueElement.querySelector(".jira-key").textContent = issue.key;
         issueElement.querySelector(".jira-summary").textContent = issue.summary;
         issueElement.querySelector(".jira-status").textContent = issue.status;
