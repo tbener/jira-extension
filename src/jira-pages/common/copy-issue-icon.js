1eaad72920ecacc63af70ed6995a51559c04c329
tal.bener <tal.bener@mdclone.com>
2025-10-07 00:50:26 +0300

    Refactor CopyIssueIcon colors to use CSS custom properties for light and dark modes

--- src/jira-pages/common/copy-issue-icon.js
+++ src/jira-pages/common/copy-issue-icon.js
@@ -67,8 +67,8 @@ export class CopyIssueIcon {
         const copyLinkSvg = `
             <svg height="${iconSize}" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g transform="translate(0 -1028.4)">
-                <path d="m12 1028.4 4 9 8 1-6 5 2 9-8-5-8 5 2-9-6-5 8-1z" fill="#f39c12"/>
-                <path d="m12 1028.4-4 9-6.9688 0.8 4.9688 4.2-0.1875 0.8 0.1875 0.2-1.75 7.8 7.75-4.8 7.75 4.8-1.75-7.8 0.188-0.2-0.188-0.8 4.969-4.2-6.969-0.8-4-9z" fill="#f1c40f"/>
+                <path d="m12 1028.4 4 9 8 1-6 5 2 9-8-5-8 5 2-9-6-5 8-1z" fill="var(--star-outer-color)"/>
+                <path d="m12 1028.4-4 9-6.9688 0.8 4.9688 4.2-0.1875 0.8 0.1875 0.2-1.75 7.8 7.75-4.8 7.75 4.8-1.75-7.8 0.188-0.2-0.188-0.8 4.969-4.2-6.969-0.8-4-9z" fill="var(--star-inner-color)"/>
                 </g>
                 <g fill="currentColor" fill-rule="evenodd" transform="${transform}">
                 ${jiraPaths}
@@ -79,8 +79,6 @@ export class CopyIssueIcon {
 
         return copyLinkSvg;
     }
-
-
 }
 
 const iconSize = 18;
